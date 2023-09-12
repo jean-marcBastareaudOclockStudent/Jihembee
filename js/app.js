11e61459b2f6e94b59ac7994bff9da3b17894d09
Jean-marc Bastareaud <jean-marc@oclock.school>
2023-09-12 20:05:25 +0200

    1st commit

---
+++ js/app.js
@@ -0,0 +1,89 @@
+// const fruitsDetails = [
+//     {
+//       nom: "banane",
+//       couleur: "jaune",
+//     },
+//     {
+//       nom: "fraise",
+//       couleur: "rouge",
+//     },
+//     {
+//       nom: "pomme",
+//       couleur: "rouge",
+//     },
+//     {
+//       nom: "melon",
+//       couleur: "orange",
+//     },
+//     {
+//       nom: "grenade",
+//       couleur: "rouge",
+//     },
+//   ];
+
+//   let i = 0;
+//    while (i < fruitsDetails.length) {
+//     const fruit = fruitsDetails[i];
+//     console.log(`Fruit : ${fruit.nom}, Couleur ${fruit.couleur}`);
+//     i++;
+//    }
+   
+// const fruitsDetailsBis = [
+//     {
+//       nom: "banane",
+//       couleur: "jaune",
+//     },
+//     {
+//       nom: "fraise",
+//       couleur: "rouge",
+//     },
+//     {
+//       nom: "pomme",
+//       couleur: "rouge",
+//     },
+//     {
+//       nom: "melon",
+//       couleur: "orange",
+//     },
+//     {
+//       nom: "grenade",
+//       couleur: "rouge",
+//     },
+//   ];
+
+//   let u = 0;
+//   while(u < fruitsDetailsBis.length) {
+//     const fruit = fruitsDetailsBis[u];
+//     console.log(`${fruit.nom}`);
+//     u++;
+//   }
+
+  const fruitsDetailsTer = [
+    {
+      nom: "banane",
+      couleur: "jaune",
+    },
+    {
+      nom: "fraise",
+      couleur: "rouge",
+    },
+    {
+      nom: "pomme",
+      couleur: "rouge",
+    },
+    {
+      nom: "melon",
+      couleur: "orange",
+    },
+    {
+      nom: "grenade",
+      couleur: "rouge",
+    },
+  ];
+
+  let y = 0;
+
+  while(y < fruitsDetailsTer.length) {
+    fruit = fruitsDetailsTer[y];
+    console.log(fruit[2]);
+  }
